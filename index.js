53724c4de2344ef6410bfc6bb1d87aebf288388a
Hannes Petri <mail@hannesp.se>
2018-01-21 14:21:54 +0100

    Limited numer of rooms

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ const uuid = require('uuid');
 
 const Room = require('./src/room');
 
-const app = express();
-
 const rooms = {};
 
+const maxNumberOfRooms = 2;
+
+const app = express();
+
 app.get('/avtoralli/rooms/new', (req, res) => {
+    if (Object.keys(rooms).length >= maxNumberOfRooms) {
+        res.status(403).send('Too many games running. Try again later!');
+        return;
+    }
+
     const id = uuid.v4();
     rooms[id] = new Room(2);
     res.redirect('/avtoralli/rooms/' + id);
@@ -42,6 +49,12 @@ wsServer.on('request', req => {
         
         if (resource !== 'rooms' || !(id in rooms)) {
             req.reject();
+            return;
+        }
+
+        if (Object.keys(rooms).length > maxNumberOfRooms) {
+            req.reject();
+            return;
         }
         
         const conn = req.accept('echo-protocol', req.origin);
@@ -49,25 +62,26 @@ wsServer.on('request', req => {
         
         if (room.isDiscontinued) {
             req.reject();
-        } else {
-            room.connect(conn);
-        
-            conn.on('message', msg => {
-                try {
-                    const {type, params} = JSON.parse(msg.utf8Data);
-                    room.handleCommand(conn, type, params || {});
-                } catch (err) {
-                    console.error(err);
-                }
-            });
-            
-            conn.on('close', (code, desc) => {
-                room.disconnect(conn);
-                if (room.isDiscontinued) {
-                    delete rooms[id];
-                }
-            });
+            return;
         }
+
+        room.connect(conn);
+        
+        conn.on('message', msg => {
+            try {
+                const {type, params} = JSON.parse(msg.utf8Data);
+                room.handleCommand(conn, type, params || {});
+            } catch (err) {
+                console.error(err);
+            }
+        });
+        
+        conn.on('close', (code, desc) => {
+            room.disconnect(conn);
+            if (room.isDiscontinued) {
+                delete rooms[id];
+            }
+        });
     } catch (err) {
         console.error('Request failed: ' + err);
     }
